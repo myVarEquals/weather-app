0609bde1733bd9794834131d3eb1d6825fd9b45a
Roger B <myvarequals@gmail.com>
2018-07-05 09:16:07 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
-import Weather from './components/Weather'
+import Weather from './components/Weather';
 
 const API_KEY = 'd6cacabe8d6ebebebba534d4b6435938';
 
 
 class App extends React.Component {
 
-  getWeather = async () => {
-    const API_CALL = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+  getWeather = async (e) => {
+    e.preventDefault();
 
-    
+    const city = e.target.elements.name.city.value;
+    const country = e.target.elements.name.country.value;
+    const API_CALL = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const data = await API_CALL.json();
+    console.log(data);
+
   }
 
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
